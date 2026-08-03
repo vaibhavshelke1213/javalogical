c94c23a799b08920d593c30d3f90625633d187dd
sambh <sambh@samlappy>
2026-08-03 16:12:21 +0530

    Intial Commit

---
+++ src/com/nit/patterns/SimpleRectanglePattern.java
@@ -0,0 +1,32 @@
+package com.nit.patterns;
+
+public class SimpleRectanglePattern 
+{
+	public static void main(String[] args) 
+	{
+		
+		for(int i = 0; i<5;i++)
+		{
+			for(int j = 0; j<5; j++)
+			{
+				System.out.print("*"+" ");
+			}
+			
+			System.out.println();
+		}
+		
+		
+	}
+	
+	/*
+	    output :-
+	 
+				  * * * * * 
+				  * * * * * 
+				  * * * * * 
+				  * * * * * 
+				  * * * * * 
+	 
+	 */
+
+}
---
+++ src/com/nit/patterns/TranglePatternDemo1.java
@@ -0,0 +1,23 @@
+package com.nit.patterns;
+
+
+public class TranglePatternDemo1 
+{
+	public static void main(String[] args) 
+	{
+		for(int i=1; i<=5;i++)
+		{
+			for(int j =0; j<i; j++)
+			{
+				System.out.print("* ");
+				
+			}
+			
+			System.out.println();
+		}
+		
+		
+	}
+	
+
+}
